2b4aaa6218a3adf9ec6ebb6cb84e435ddc551c20
Markus Mayer <widemeadows@gmail.com>
2022-03-10 17:12:19 +0100

    Initial commit

---
+++ src/BlazorDesktop/Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using BlazorDesktop.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+builder.Services.AddHsts(hsts => hsts.MaxAge = TimeSpan.FromMinutes(15)); // The default HSTS value is 30 days.
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
