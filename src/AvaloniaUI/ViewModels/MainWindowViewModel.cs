4dcd31f7d08412e4335305b462d4d5b75469969e
Markus Mayer <widemeadows@gmail.com>
2022-03-10 18:24:02 +0100

    Add reactive binding in Main Window

--- src/AvaloniaUI/ViewModels/MainWindowViewModel.cs
+++ src/AvaloniaUI/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,22 @@
-﻿namespace AvaloniaUI.ViewModels;
+﻿using System.Windows.Input;
+using ReactiveUI;
+
+namespace AvaloniaUI.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting => "Welcome to Avalonia!";
+    private int _count;
+
+    public MainWindowViewModel()
+    {
+        AddOneCommand = ReactiveCommand.Create(() => { Count++; });
+    }
+
+    public int Count
+    {
+        get => _count;
+        private set => this.RaiseAndSetIfChanged(ref _count, value);
+    }
+
+    public ICommand AddOneCommand { get; }
 }
